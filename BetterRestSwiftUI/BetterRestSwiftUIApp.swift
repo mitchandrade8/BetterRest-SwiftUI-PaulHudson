38a70429a01eec1a09c01b74c4b08bea5bf36097
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2022-03-06 17:38:22 -0700

    Initial Commit

---
+++ BetterRestSwiftUI/BetterRestSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestSwiftUIApp.swift
+//  BetterRestSwiftUI
+//
+//  Created by Mitch Andrade on 3/6/22.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BetterRestSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BetterRestSwiftUI
+//
+//  Created by Mitch Andrade on 3/6/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
